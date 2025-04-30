2a5cae353cc06278314d0439f58a6861bae901f9
TPD94 <tpd94@cdrm-project.com>
2025-04-30 05:11:05 -0400

    Remove icon links

--- configs/icon_links.py
+++ configs/icon_links.py
@@ -1,5 +1,5 @@
 data = {
-    'discord': 'https://discord.cdrm-project.com/',
-    'telegram': 'https://telegram.cdrm-project.com/',
-    'gitea': 'https://cdm-project.com/tpd94/cdm-project'
+    'discord': '#',
+    'telegram': '#',
+    'gitea': '#'
 }
